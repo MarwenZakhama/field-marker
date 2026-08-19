cb9d97152c466de8591442ef05535498e1481cbc
MarwenZakhama <103939628+MarwenZakhama@users.noreply.github.com>
2026-08-19 21:23:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE = "fieldmarker-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./piexif.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).then(function(resp){
+        var copy = resp.clone();
+        caches.open(CACHE).then(function(cache){ cache.put(event.request, copy); });
+        return resp;
+      }).catch(function(){ return cached; });
+    })
+  );
+});
